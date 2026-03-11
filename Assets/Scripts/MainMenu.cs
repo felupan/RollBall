419989d6325d6f067fa52839b375890280211e61
Felipe Roncagliolo <felipealicantino@gmail.com>
2026-03-11 23:39:02 +0100

    afa

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,13 +1,23 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private TMP_Text titleText;
+    
+    private string currentTitleText;
+    private string copyTitleText;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        copyTitleText = titleText.text;   
+        PlayTitle();
     }
 
     // Update is called once per frame
@@ -24,6 +34,23 @@ public class MainMenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    void PlayTitle()
+    {
+        StartCoroutine(PlayTitleCoroutine());
+    }
+
+    IEnumerator PlayTitleCoroutine()
+    {
+        List<string> words = new List<string>(copyTitleText.Split(" "));
+    
+        foreach (var word in words)
+        {
+            currentTitleText += word + " ";
+            titleText.SetText(currentTitleText);
+            yield return new WaitForSeconds(0.3f);
+        }
     }
     
 }
